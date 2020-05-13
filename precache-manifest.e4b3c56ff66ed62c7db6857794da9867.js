46bdab3ffddd3c09af4618d8fcf236ad020da990
a510102 <44679115+a510102@users.noreply.github.com>
2020-05-14 01:37:07 +0800

    Updates

---
+++ precache-manifest.e4b3c56ff66ed62c7db6857794da9867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55b21c2302e364ec5d63692dfde8d09",
+    "url": "/news-web/index.html"
+  },
+  {
+    "revision": "aaf1af3552d1b74a14bd",
+    "url": "/news-web/static/css/main.1404baab.chunk.css"
+  },
+  {
+    "revision": "a727dd244717fe9e61c7",
+    "url": "/news-web/static/js/2.0fa5118b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news-web/static/js/2.0fa5118b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf1af3552d1b74a14bd",
+    "url": "/news-web/static/js/main.e1c36397.chunk.js"
+  },
+  {
+    "revision": "ffed06a1cf4b38f0bff6",
+    "url": "/news-web/static/js/runtime-main.37616d71.js"
+  }
+]);
